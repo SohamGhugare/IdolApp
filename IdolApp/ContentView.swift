5ee8271f7c703e43a66a156d0d64575231f55b51
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-02 12:23:33 +0530

    feat: added scrollview

--- IdolApp/ContentView.swift
+++ IdolApp/ContentView.swift
@@ -10,10 +10,44 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Explore Idols")
+                .foregroundStyle(.black)
+                .fontWeight(.bold)
+                .font(.system(size: 45))
+                .padding(.bottom, 80)
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(0..<10) {x in
+                        
+                        
+                        HStack {
+                            Spacer()
+                            Image(systemName: "globe")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                            Spacer()
+                            Text("Item \(x)")
+                                .foregroundStyle(.black)
+                                .font(.largeTitle)
+                            Spacer()
+                                
+                        }
+//                        .padding(.horizontal, 20)
+                        .frame(width: 300, height: 150)
+                        .background(.white)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(.black, lineWidth: 3)
+                        }
+                       
+                        
+                        
+                        
+                    }
+                }
+            } .frame(width: 400, height: 500)
         }
         .padding()
     }
