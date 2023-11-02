acc5cf0fe74d22916ec91071743d5d209e01362f
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-02 11:52:16 +0530

    initial commit

---
+++ IdolApp/IdolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdolAppApp.swift
+//  IdolApp
+//
+//  Created by Soham Ghugare on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IdolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
